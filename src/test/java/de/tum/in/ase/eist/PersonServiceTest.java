dbb746b15a3d561888611888269121f423cdf7c0
ge32goy <hanyin.chen@tum.de>
2022-07-12 23:35:20 +0200

    init

--- src/test/java/de/tum/in/ase/eist/PersonServiceTest.java
+++ src/test/java/de/tum/in/ase/eist/PersonServiceTest.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 
 import java.time.LocalDate;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -59,13 +60,78 @@ class PersonServiceTest {
 //        Verify that all instances of Person have been correctly saved to the database
 //        and that the parent-child relationship has been created only for the first two added parents
 //        (in the database and the returned object of addParent).
-        var person = new Person();
-        person.setFirstName("Max");
-        person.setLastName("Mustermann");
-        person.setBirthday(LocalDate.now());
+        var personParent = new Person();
+        personParent.setFirstName("Max");
+        personParent.setLastName("Mustermann");
+        personParent.setBirthday(LocalDate.of(1990, 2, 18));
 
-        personService.save(person);
+        var personChild = new Person();
+        personChild.setFirstName("Jonas");
+        personChild.setLastName("Mustermann");
+        personChild.setBirthday(LocalDate.now());
 
-        assertEquals(1, personRepository.findAll().size());
+        Person parentSavedDatabase = personService.save(personParent);
+        Person childSavedDatabase = personService.save(personChild);
+
+        Person childSaved = personService.addParent(personChild, personParent);
+        Set<Person> parentSet = childSaved.getParents();
+
+        Set<Person> childrenSetDatabase = parentSavedDatabase.getChildren();
+        Set<Person> parentSetDatabase = childSavedDatabase.getParents();
+
+        assertEquals(2, personRepository.findAll().size());
+        assertTrue(personChild.equals(childSaved));
+        assertEquals(parentSet, parentSetDatabase);
+    }
+
+    @Test
+    void testAddThreeParents() {
+//        Create four instances of Person,
+//        one acts as a child and three as parents,
+//        and save these objects to the database.
+//        Invoke the method addParent twice
+//        so that the parent-child relationships between the two instances of Person are created.
+//        Assert that the method invocations return the correct persons,
+//        the parent and child have been correctly saved to the database,
+//        and that the parent-child relationship is created (in the database
+//        and the returned objects of addParent).
+        var personParent1 = new Person();
+        personParent1.setFirstName("Max");
+        personParent1.setLastName("Mustermann");
+        personParent1.setBirthday(LocalDate.of(1990, 2, 18));
+
+        var personParent2 = new Person();
+        personParent2.setFirstName("Shawn");
+        personParent2.setLastName("Mendes");
+        personParent2.setBirthday(LocalDate.of(1995, 4, 9));
+
+        var personParent3 = new Person();
+        personParent3.setFirstName("Yamashita");
+        personParent3.setLastName("Tomohisa");
+        personParent3.setBirthday(LocalDate.of(1991, 9, 1));
+
+        var personChild = new Person();
+        personChild.setFirstName("Jonas");
+        personChild.setLastName("Mustermann");
+        personChild.setBirthday(LocalDate.now());
+
+        Person parent1SavedDatabase = personService.save(personParent1);
+        Person parent2SavedDatabase = personService.save(personParent2);
+        Person parent3SavedDatabase = personService.save(personParent3);
+        Person childSavedDatabase = personService.save(personChild);
+
+        Person childSaved = personService.addParent(personChild, personParent1);
+        Set<Person> childrenSet = childSaved.getParents();
+        Person child2Saved = personService.addParent(personChild, personParent2);
+        Set<Person> childrenSet2 = child2Saved.getParents();
+
+        Set<Person> childrenSetDatabase = parent1SavedDatabase.getChildren();
+        Set<Person> parentSet = childSavedDatabase.getParents();
+
+        assertEquals(4, personRepository.findAll().size());
+        assertEquals(personParent1, parent1SavedDatabase);
+        assertEquals(personParent2, parent2SavedDatabase);
+        assertTrue(personChild.getParents().contains(personParent1));
+        assertTrue(personChild.getParents().contains(personParent2));
     }
 }
